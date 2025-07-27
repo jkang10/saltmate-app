9607279c576ee1b3aa3b2a6835d720f1fa08e963
junho jang <jkang10@naver.com>
2025-07-27 19:46:15 +0900

    Fix publicPath for Netlify deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require("@vue/cli-service");
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  publicPath: '/' // 여기에 중요! 빌드된 파일이 제공될 기본 URL 경로.
+})
