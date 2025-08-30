e2122c20ad59c7162d2ad7d2f13b0f4598e9ef97
junho jang <jkang10@naver.com>
2025-08-30 20:41:54 +0900

    수정 업데이트

--- vue.config.js
+++ vue.config.js
@@ -10,10 +10,12 @@ module.exports = defineConfig({
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
 
-    workboxPluginMode: 'InjectManifest',
+    // [핵심 수정] 서비스 워커 생성 모드를 'GenerateSW'로 변경합니다.
+    workboxPluginMode: 'GenerateSW', 
     workboxOptions: {
-      // [핵심 수정] 경로를 './'로 시작하는 상대 경로로 변경합니다.
-      swSrc: './src/firebase-messaging-sw.js'
+      // [핵심 수정] InjectManifest 대신, 생성될 서비스 워커에
+      // 우리 F-CM 파일을 import하도록 설정합니다.
+      importScripts: ['firebase-messaging-sw.js']
     }
   }
 });
