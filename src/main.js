5feaf0641b2bd4301e7375febad86a3ae84a64e9
junho jang <jkang10@naver.com>
2025-09-28 11:18:25 +0900

    수정 업데이트

--- src/main.js
+++ src/main.js
@@ -3,11 +3,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-// [핵심] functions를 firebaseConfig에서 직접 import 합니다.
-import { auth, functions } from "./firebaseConfig";
+import { auth } from "./firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
-// [핵심] httpsCallable을 firebase/functions에서 직접 import 합니다.
-import { httpsCallable } from "firebase/functions";
 
 // ECharts 관련 코드는 그대로 둡니다.
 import ECharts from "vue-echarts";
@@ -37,10 +34,5 @@ onAuthStateChanged(auth, () => {
     app = createApp(App);
     app.component("v-chart", ECharts);
     app.use(router).mount("#app");
-
-    // [디버깅용 코드 추가] 아래 두 줄을 추가해주세요.
-    // 앱이 사용하는 functions와 httpsCallable을 window 객체에 할당하여 콘솔에서 접근할 수 있게 합니다.
-    window.f_functions = functions;
-    window.f_httpsCallable = httpsCallable;
   }
 });
