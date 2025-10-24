8b70edf0e50e9fa89dcd3e52b8e1528267a77d3d
junho jang <jkang10@naver.com>
2025-10-24 13:12:04 +0900

    수정 업데이트

--- src/components/UserInfoWidget.vue
+++ src/components/UserInfoWidget.vue
@@ -227,20 +227,6 @@ const emitOpenNotificationSettingsModal = () => {
   emit('openNotificationSettingsModal');
 };
 
-// Service Worker 등록 함수 (필요 시 사용)
-async function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
-      console.log('Service Worker 등록 성공:', registration);
-      return registration;
-    } catch (error) {
-      console.error('Service Worker 등록 실패:', error);
-      throw error;
-    }
-  }
-  throw new Error('Service workers are not supported in this browser.');
-}
 </script>
 
 <style scoped>
