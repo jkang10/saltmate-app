5ed973d049ed0671ee329220ddf5b9fdd3a6b5de
junho jang <jkang10@naver.com>
2025-10-26 09:18:19 +0900

    수정 업데이트

--- src/views/SoleinDigitalUtopia.vue
+++ src/views/SoleinDigitalUtopia.vue
@@ -623,7 +623,7 @@ const handleResize = () => {
 // --- 컴포넌트 라이프사이클 훅 ---
 onMounted(async () => {
   // ▼▼▼ [새로운 테스트 코드] 이 라인을 onMounted 최상단에 추가하세요 ▼▼▼
-  console.log('--- DEPLOY TEST VERSION 4 --- THIS IS THE NEWEST CODE ---');
+  console.log('--- DEPLOY TEST VERSION 10 --- THIS IS THE NEWEST CODE ---');
   // ▲▲▲ [새로운 테스트 코드] ▲▲▲
 
   // 로그인 확인
@@ -656,9 +656,17 @@ onMounted(async () => {
       myAvatar = await loadAvatar(myAvatarUrl);
 
       // ▼▼▼ [최종 디버깅] ▼▼▼
-      console.log('--- MY AVATAR OBJECT DEBUG ---');
+      console.log('--- MY AVATAR OBJECT DEBUG (Before Reset) ---');
       console.log(myAvatar);
       // ▲▲▲ [최종 디버깅] ▲▲▲
+
+      // --- ▼▼▼ [핵심 수정] ---
+      // 로드된 아바타의 위치가 (0,0,0)이 아닐 경우를 대비하여
+      // 씬에 추가하기 직전에 강제로 위치를 초기화합니다.
+      myAvatar.position.set(0, 0, 0); 
+      myAvatar.rotation.set(0, 0, 0); // 회전도 초기화 (안전을 위해)
+      console.log('--- AVATAR POSITION FORCED TO (0,0,0) ---');
+      // --- ▲▲▲ [핵심 수정] ---
 
       scene.add(myAvatar); // 씬에 아바타 추가
       if (myUserName) { // 이름이 있으면 닉네임 추가
