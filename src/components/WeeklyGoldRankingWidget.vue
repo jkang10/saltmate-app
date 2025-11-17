a1f335a5829f1c8781c14f64b68ac4f79d8b9e6a
junho jang <jkang10@naver.com>
2025-11-17 20:33:54 +0900

    랭킹 수정 업데이트

--- src/components/WeeklyGoldRankingWidget.vue
+++ src/components/WeeklyGoldRankingWidget.vue
@@ -82,7 +82,7 @@ onMounted(async () => {
   height: 200px;
 
   /* ▼▼▼ [★핵심 수정 2★] 'auto' 대신 고정 여백을 다시 사용합니다. (겹치지 않을 만큼) ▼▼▼ */
-  margin-top: 25px; 
+  margin-top: 45px; 
   /* ▲▲▲ (수정 완료) ▲▲▲ */
 }
 
@@ -134,7 +134,7 @@ onMounted(async () => {
 }
 .loading-spinner {
   /* ▼▼▼ [★핵심 수정 4★] 'auto' 마진을 일반 마진으로 되돌립니다. ▼▼▼ */
-  margin: 20px auto; 
+  margin: 40px auto; 
   /* ▲▲▲ (수정 완료) ▲▲▲ */
   
   display: block;
